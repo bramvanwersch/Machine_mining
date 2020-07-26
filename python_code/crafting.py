93848af43d738355969bf2635695ddf102a5d1ed
Bram <eckelradekip10@gmail.com>
2020-07-26 13:10:56 +0100

    added documentation to the crafing methods

--- python_code/crafting.py
+++ python_code/crafting.py
@@ -6,19 +6,36 @@ from python_code.event_handling import EventHandler
 from python_code.widgets import Frame, Label, ScrollPane, ItemLabel
 
 class CraftingInterface(EventHandler):
+    """
+    Contains all the crafting GUI and all associated methods
+    """
     def __init__(self, terminal_inventory, *groups):
         EventHandler.__init__(self, [])
         self.__window = CraftingWindow(terminal_inventory, *groups)
 
     def show(self, value):
+        """
+        Toggle showing the crafting window or not. This also makes sure that no
+        real updates are pushed while the window is invisible
+
+        :param value: a boolean
+        """
         self.__window.visible = value
 
     def handle_events(self, events):
+        """
+        Handle events issued by the user not consumed by the Main module
+
+        :param events: a list of events
+        """
         leftovers = super().handle_events(events)
         if self.__window.visible:
             self.__window.handle_events(events)
 
 class CraftingWindow(Frame):
+    """
+    A Frame for the crafting GUI
+    """
     COLOR = (173, 94, 29, 150)
     GRID_SIZE = Size(9, 9)
     GRID_PIXEL_SIZE = Size(450, 450)
@@ -33,40 +50,44 @@ class CraftingWindow(Frame):
         self._crafting_grid = [[]]
         self.__inventory = terminal_inventory
         self.__no_items = self.__inventory.number_of_items
+
+        #just ti signify that this exists
         self.__inventory_sp = None
         self.__innitiate_widgets()
 
-
     def update(self, *args):
-        super().update(*args)
-        if self.__no_items != self.__inventory.number_of_items:
-            self.__no_items = self.__inventory.number_of_items
-            self.__check_item_labels()
+        """
+        Entity update method, add labels to the scroll pane when needed.
 
-    def __check_item_labels(self):
+        :See: Entity.update()
+        """
+        super().update(*args)
+        if self.__no_items < self.__inventory.number_of_items:
+            self.__no_items = self.__inventory.number_of_items
+            self.__add_item_labels()
+
+    def __add_item_labels(self):
+        """
+        When more different items are encountered then previously in the
+        inventory a new label is added for an item. The labels are added to the
+        scrollpane
+
+        First it is figured out what items are new and then a label for each is
+        constructed
+        """
         covered_items = [widget.item.name for widget in self._inventory_sp.widgets]
         for item in self.__inventory.items:
             if item.name not in covered_items:
-                #create a background surface
-                image = pygame.Surface((44, 44))
-                image.fill(self.COLOR[:-1])
-
-                #get the item image and place it in the center
-                center = pygame.transform.scale(item.surface, (30,30))
-                image.blit(center, (44 / 2 - 30 / 2 , 44 / 2 - 30 / 2))
-
-                #draw rectangle slightly smaller then image
-                rect = image.get_rect()
-                rect.inflate_ip(-4, -4)
-                pygame.draw.rect(image, (0,0,0), rect, 3)
-
-                #add the ItemLabel to the inventory
-                lbl = ItemLabel((0, 0), (44, 44), item, image=image)
+                #remove the alpha channel
+                lbl = ItemLabel((0, 0), (44, 44), item, color=self.COLOR[:-1])
                 self._inventory_sp.add_widget(lbl)
 
 
     def __innitiate_widgets(self):
-
+        """
+        Innitiate all the widgets neccesairy for the crafting window at the
+        start
+        """
         #create grid
         start_pos = [25, 50]
         background_lbl = Label(start_pos, self.GRID_PIXEL_SIZE, color = (0,0,0, 150))
@@ -83,5 +104,5 @@ class CraftingWindow(Frame):
             self._crafting_grid.append(row)
 
         #create scrollable inventory
-        self._inventory_sp  = ScrollPane((500, 50), (175, 450), (175, 800), color=self.COLOR)
+        self._inventory_sp  = ScrollPane((500, 50), (175, 450), (175, 800), color=self.COLOR[:-1])
         self.add_widget(self._inventory_sp)
