bfa0e9bd0dfb61cd413efc19d3f9b303f6921da2
Bram <eckelradekip10@gmail.com>
2020-12-28 10:29:30 +0100

    innitial structure generation is now in an area around the start instead of all of it

--- python_code/board_generation/generation.py
+++ python_code/board_generation/generation.py
@@ -11,60 +11,96 @@ import board_generation.biomes as biome_classes
 
 class BoardGenerator:
     # the size of quadrants where a new biome is chosen
-    __BIOME_SIZES: ClassVar[Dict[str, util.Size]] = \
+    BIOME_SIZES: ClassVar[Dict[str, util.Size]] = \
         {"tiny": util.Size(100, 100), "small": util.Size(200, 200), "normal": util.Size(500, 500),
          "big": util.Size(750, 7500), "massive": util.Size(1000, 1000), "huge": util.Size(1500, 1500)}  # in blocks
 
     # determines the standard deviation of the biomes, high values means very broad distributions
-    __BIOME_BLEND: ClassVar[Dict[str, int]] = \
+    BIOME_BLEND: ClassVar[Dict[str, int]] = \
         {"tiny": 1, "small": 5, "normal": 15, "severe": 30, "extreme": 50}
 
-    # CAVE values TODO change these when generating dynamic map
-    MAX_CAVES = 8
+    # CAVE values
+    MAX_CAVES: ClassVar[Dict[str, int]] = \
+        {"no_caves": 0, "low": 5, "normal": 15, "lots": 30, "I WANT CAVES!!!": 50}  # per 100_000 blocks
     # the fraction of the distance between points based on the shortest side of the board
-    POINT_FRACTION_DISTANCE = 0.35
-    # distance the center of the cave should at least be away from the border 10%
-    CAVE_X_BORDER_DISTANCE = int(0.1 * con.BOARD_SIZE.width)
-    CAVE_Y_BORDER_DISTANCE = int(0.1 * con.BOARD_SIZE.height)
+    MAX_POINT_DISTANCE: ClassVar[int] = 100
     # number of points a cave consists of
-    MAX_CAVE_POINTS = 20
-    # the chance for a cave to stop extending around its core. Do not go lower then 0.0001 --> takes a long time
-    CAVE_STOP_SPREAD_CHANCE = 0.05
+    CAVE_LENGTH: ClassVar[Dict[str, int]] = \
+        {"short": 25, "normal": 50, "long": 100, "very long": 200, "when does it stop?": 500}
+    # the chance for a cave to stop extending around its core. Do not go lower then 0.0005 --> takes a long time
+    CAVE_STOP_SPREAD_CHANCE: ClassVar[Dict[str, int]] =\
+        {"very low": 0.1, "low": 0.05, "normal": 0.02, "large": 0.008, "massive": 0.002}
 
     # BORDER values
     BORDER_SPREAD_LIKELYHOOD = util.Gaussian(0, 2)
     MAX_BORDER_SPREAD_DISTANCE = 4
 
-    __environment_material_name: Set[str]
+    __environment_material_names: Set[str]
     __biome_definition: biome_classes.BiomeGenerationDefinition
     __biome_size: util.Size
     __biome_blend: int
+    __biome_matrix: List[List[Union[biome_classes.Biome, None]]]
 
     def __init__(
         self,
         biome_generation_def: Union[biome_classes.BiomeGenerationDefinition, str] = None,
         biome_size: Union[str, util.Size] = "normal",
-        biome_blend: Union[str, int] = "normal"
+        biome_blend: Union[str, int] = "normal",
+        nr_caves: Union[str, int] = "normal",
+        cave_length: Union[str, int] = "normal",
+        cave_broadness: Union[str, float] = "normal"
     ):
         self.__environment_material_names = {mat.name() for mat in block_util.environment_materials}
+
+        # amount of cave points
+        self.__cave_length = self.CAVE_LENGTH.get(cave_length, cave_length)
+        # the square where one cave occurs
+        self.__cave_quadrant_size = self.__determine_cave_quadrant_size(self.MAX_CAVES.get(nr_caves, nr_caves))
+
+        self.__cave_stop_spread_chance = max(self.CAVE_STOP_SPREAD_CHANCE.get(cave_broadness, cave_broadness), 0.0005)
+        # minimum distance (x and y) between the closest generated chunk and the non-generated structures and biomes
+        self.__minimum_generation_length = ceil(self.__cave_length * self.MAX_POINT_DISTANCE)
+        # tracks what part of the board the structures and biomes have been generated for.
+        self.__structure_biome_generation_rectangle = \
+            pygame.Rect((con.START_CHUNK_POS[0] * con.CHUNK_SIZE.width,
+                         con.START_CHUNK_POS[1] * con.CHUNK_SIZE.height, 0, 0))
+
+        innit_generation_rect = pygame.Rect(
+            (max(0, con.START_LOAD_AREA[0][0] * con.CHUNK_SIZE.width - self.__minimum_generation_length),
+             max(0, con.START_LOAD_AREA[1][0] * con.CHUNK_SIZE.height - self.__minimum_generation_length),
+             min(len(con.START_LOAD_AREA[0]) * con.CHUNK_SIZE.width + 2 * self.__minimum_generation_length,
+                 con.BOARD_SIZE.width),
+             min(len(con.START_LOAD_AREA[1]) * con.CHUNK_SIZE.height + 2 * self.__minimum_generation_length,
+                 con.BOARD_SIZE.height))
+        )
         self.__biome_definition = biome_generation_def if biome_generation_def else\
             biome_classes.NormalBiomeGeneration()
-        self.__biome_size = self.__BIOME_SIZES[biome_size] if biome_size in self.__BIOME_SIZES else biome_size
-        self.__biome_blend = self.__BIOME_BLEND[biome_blend] if biome_blend in self.__BIOME_BLEND else biome_blend
+        self.__biome_size = self.BIOME_SIZES.get(biome_size, biome_size)
+        self.__biome_blend = self.BIOME_BLEND.get(biome_blend, biome_blend)
+        # fill the biome matrix with empty values
+        self.__biome_matrix = [[None for _ in range(ceil(con.BOARD_SIZE.width / self.__biome_size.width))]
+                               for _ in range(ceil(con.BOARD_SIZE.height / self.__biome_size.height))]
+        self.__generate_biomes(innit_generation_rect)
 
-        # TODO make these dynamically grow when new areas are loaded
-        self.__biome_matrix = self.__generate_biomes()
         self.__predefined_blocks = PredefinedBlocks()
-        self.__generate_structure_locations()
+        self.__generate_structure_locations(innit_generation_rect)
 
-    def __generate_biomes(self) -> List[List[biome_classes.Biome]]:
-        biome_matrix = []
+    def __determine_cave_quadrant_size(self, caves_nr: int) -> util.Size:
+        total_blocks = (con.BOARD_SIZE.width / con.BLOCK_SIZE.width) * (con.BOARD_SIZE.height / con.BLOCK_SIZE.height)
+        total_caves = (total_blocks / 100_000) * caves_nr
+        # Me brother : )
+        cave_quadrant_side = int(sqrt((con.BOARD_SIZE.width * con.BOARD_SIZE.height) / total_caves))
+        return util.Size(cave_quadrant_side, cave_quadrant_side)
 
+    def __generate_biomes(self, rect) -> None:
+        row_start = int(rect.top / self.__biome_size.height)
+        col_start = int(rect.left / self.__biome_size.width)
         # make sure that even partial areas of the board are covered by a biome
-        for row_i in range(ceil(con.BOARD_SIZE.height / self.__biome_size.height)):
-            row = []
-            for col_i in range(ceil(con.BOARD_SIZE.width / self.__biome_size.width)):
-                # allow the shapes of the distributions to be a bit different
+        for row_i in range(ceil(rect.height / self.__biome_size.height)):
+            for col_i in range(ceil(rect.width / self.__biome_size.width)):
+                row_i += row_start
+                col_i += col_start
+                # allow the shapes of the distributions to be a bit different (more oval)
                 sd_x = self.__biome_size.width * self.__biome_blend * uniform(0.6, 1.4)
                 sd_y = self.__biome_size.height * self.__biome_blend * uniform(0.6, 1.4)
                 # allow the distribution to be tilted
@@ -76,35 +112,23 @@ class BoardGenerator:
                 # check depths in blocks
                 biome_type = self.__biome_definition.get_biome(int(mean_y / con.BLOCK_SIZE.height))
                 biome_instance = biome_type(util.Gaussian(mean_x, sd_x), util.Gaussian(mean_y, sd_y), cov1, cov2)
-                row.append(biome_instance)
-            biome_matrix.append(row)
-        return biome_matrix
+                self.__biome_matrix[row_i][col_i] = biome_instance
 
-    def __generate_biome(self, tl_x, tl_y) -> biome_classes.Biome:
-        # allow the shapes of the distributions to be a bit different
-        sd_x = self.__biome_size.width * self.__biome_blend * uniform(0.8, 1.2)
-        sd_y = self.__biome_size.height * self.__biome_blend * uniform(0.8, 1.2)
-        # make sure that distribution centers are not right at the edge of the quadrant
-        mean_x = randint(tl_x, int(tl_x + self.__biome_size.width / 2))
-        mean_y = randint(tl_y, int(tl_y + self.__biome_size.height / 2))
-        # check depths in blocks
-        biome_type = self.__biome_definition.get_biome(int(mean_y / con.BLOCK_SIZE.height))
-        biome_instance = biome_type(util.Gaussian(mean_x, sd_x), util.Gaussian(mean_y, sd_y))
-        return biome_instance
-
-    def __generate_structure_locations(self):
-        """generate structures in a given rectangle by adding blocks to the __predefined_blocks tree"""
-        # TODO add structures here
-        nr_caves = randint(self.MAX_CAVES - int(max(self.MAX_CAVES / 2, 1)), self.MAX_CAVES)
-        # Me brother : )
-        square_size = sqrt((con.BOARD_SIZE.width * con.BOARD_SIZE.height) / nr_caves)
-        for row_i in range(round(con.BOARD_SIZE.height / square_size)):
-            for col_i in range(round(con.BOARD_SIZE.width / square_size)):
-                x_coord = randint(int(col_i * square_size), int((col_i + 1) * square_size))
-                y_coord = randint(int(row_i * square_size), int((row_i + 1) * square_size))
+    def __generate_structure_locations(self, rect):
+        row_start = int(rect.top / self.__cave_quadrant_size.height)
+        col_start = int(rect.left / self.__cave_quadrant_size.width)
+        for row_i in range(ceil(rect.height / self.__cave_quadrant_size.height)):
+            for col_i in range(ceil(rect.width / self.__cave_quadrant_size.width)):
+                row_i += row_start
+                col_i += col_start
+                x_coord = randint(int(col_i * self.__cave_quadrant_size.height),
+                                  int((col_i + 1) * self.__cave_quadrant_size.height))
+                y_coord = randint(int(row_i * self.__cave_quadrant_size.width),
+                                  int((row_i + 1) * self.__cave_quadrant_size.width))
                 self.__generate_cave([x_coord, y_coord])
+        # TODO add structures here, after the caves
 
-    def biome_liklyhoods_from_point(self, x, y):
+    def __biome_liklyhoods_from_point(self, x, y):
         biome_matrix_col = int(x / self.__biome_size.width)
         biome_matrix_row = int(y / self.__biome_size.height)
         main_biome = self.__biome_matrix[biome_matrix_row][biome_matrix_col]
@@ -116,7 +140,7 @@ class BoardGenerator:
                     or surrounding_coord[1] >= len(self.__biome_matrix) - 1 or surrounding_coord[1] < 0:
                 continue
             surrounding_biomes.append(self.__biome_matrix[surrounding_coord[1]][surrounding_coord[0]])
-        wheights = util.normalize([b.get_likelyhood_at_coord(x, y) for b in surrounding_biomes])
+        wheights = util.normalize([b.get_likelyhood_at_coord(x, y) for b in surrounding_biomes if b is not None])
         biome_likelyhoods = {biome: wheights[index] for index, biome in enumerate(surrounding_biomes)}
         return biome_likelyhoods
 
@@ -151,8 +175,8 @@ class BoardGenerator:
                 block_y_coord = int(rect.top / con.BLOCK_SIZE.height) + row_i
                 block = matrix[row_i][col_i]
                 # determine biome based on coordinate
-                biome_liklyhoods = self.biome_liklyhoods_from_point(block_x_coord * con.BLOCK_SIZE.width,
-                                                                    block_y_coord * con.BLOCK_SIZE.height)
+                biome_liklyhoods = self.__biome_liklyhoods_from_point(block_x_coord * con.BLOCK_SIZE.width,
+                                                                      block_y_coord * con.BLOCK_SIZE.height)
                 biome = choices(list(biome_liklyhoods.keys()), list(biome_liklyhoods.values()), k=1)[0]
                 # only add plants in caves
                 if block == "Air" and uniform(0, 1) < biome.FLORA_LIKELYHOOD:
@@ -221,7 +245,6 @@ class BoardGenerator:
         return ore_locations
 
     def __generate_cave(self, start_point):
-        """Save air_blocks in the pre-defined tree"""
         cave_points = self.__get_cave_points(start_point)
         # get the line between the points
         for index1 in range(1, len(cave_points)):
@@ -247,7 +270,7 @@ class BoardGenerator:
                      and not self.__predefined_blocks.check(coord, "Air")]
                 # extend the cave around the direct line.
                 while len(surrounding_coords) > 0:
-                    if uniform(0, 1) < self.CAVE_STOP_SPREAD_CHANCE:
+                    if uniform(0, 1) < self.__cave_stop_spread_chance:
                         break
                     remove_coord = choice(surrounding_coords)
                     surrounding_coords.remove(remove_coord)
@@ -258,19 +281,15 @@ class BoardGenerator:
                     surrounding_coords.extend(additional_surrounding_coords)
 
     def __get_cave_points(self, start_point):
-        max_radius = int(min(*con.BOARD_SIZE) * self.POINT_FRACTION_DISTANCE)
 
         cave_points = [start_point]
         prev_direction = uniform(0, 2 * pi)
-        amnt_points = randint(self.MAX_CAVE_POINTS - int(max(self.MAX_CAVE_POINTS / 2, 1)),
-                              self.MAX_CAVE_POINTS)
+        amnt_points = randint(int(max(self.__cave_length / 2, 1)), self.__cave_length)
         while len(cave_points) < amnt_points:
-            radius = randint(max(1, int(max_radius / 2)), max_radius)
+            radius = randint(max(1, int(self.MAX_POINT_DISTANCE / 2)), self.MAX_POINT_DISTANCE)
             prev_direction = uniform(prev_direction - 0.5 * pi, prev_direction + 0.5 * pi)
-            new_x = min(max(int(cave_points[-1][0] + cos(prev_direction) * radius), self.CAVE_X_BORDER_DISTANCE),
-                        con.BOARD_SIZE.width - self.CAVE_X_BORDER_DISTANCE)
-            new_y = min(max(int(cave_points[-1][1] + sin(prev_direction) * radius), self.CAVE_Y_BORDER_DISTANCE),
-                        con.BOARD_SIZE.height - self.CAVE_Y_BORDER_DISTANCE)
+            new_x = min(max(int(cave_points[-1][0] + cos(prev_direction) * radius), 0), con.BOARD_SIZE.width)
+            new_y = min(max(int(cave_points[-1][1] + sin(prev_direction) * radius), 0), con.BOARD_SIZE.height)
             # make sure no double points and no straight lines
             if [new_x, new_y] in cave_points or \
                     int(new_x / con.BLOCK_SIZE.width) == int(cave_points[-1][0] / con.BLOCK_SIZE.width) or \
