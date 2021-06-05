19858523a60677dc1eef89d9b485a704404ca659
bramvanwersch <eckelradekip10@gmail.com>
2021-06-05 10:47:44 +0200

    saving is now proper. Can save new files and overwrite old ones.

--- python_code/interfaces/windows/file_explorer_interface.py
+++ python_code/interfaces/windows/file_explorer_interface.py
@@ -19,6 +19,7 @@ class OpenFile(base_interface.Window):
         self.file_list = None
         self.__init_widgets()
         self.__create_name_popup = None
+        self.__overwrite_file_popup = None
 
     def update(self, *args):
         super().update(*args)
@@ -27,11 +28,20 @@ class OpenFile(base_interface.Window):
     def check_popups(self):
         if self.__create_name_popup is not None and not self.__create_name_popup.is_showing():
             response = self.__create_name_popup.response
-            print(response)
             self.__create_name_popup = None
             if response is not False:
-                scenes.scenes["Game"].save(response)
-                self._close_window()
+                self.save_game(response)
+        if self.__overwrite_file_popup is not None and not self.__overwrite_file_popup.is_showing():
+            response = self.__overwrite_file_popup.response
+            self.__overwrite_file_popup = None
+            if response is not False:
+                self.save_game(response)
+
+    def save_game(self, file_name):
+        # since dots are not allowed to be in file names this should be save
+        file_name = file_name.replace(".save", "")
+        scenes.scenes["Game"].save(file_name)
+        self._close_window()
 
     def __init_widgets(self):
         save_label = widgets.Label(util.Size(200, 30), text="Choose a save file:", font_size=25, color=(0, 0, 0, 0),
@@ -49,15 +59,22 @@ class OpenFile(base_interface.Window):
         self.file_list.add_widget(new_button)
 
         for file in os.listdir(con.SAVE_DIR):
-            if isfile(join(con.SAVE_DIR, file)):
+            if isfile(join(con.SAVE_DIR, file) and file.endswith(".save")):
                 file_button = widgets.Button(util.Size(self.file_list.rect.width, 25), text=file, font_size=25,
                                              color=self.COLOR)
+                file_button.add_key_event_listener(1, self.confirm_overwrite_window, values=[file], types=["unpressed"])
                 self.file_list.add_widget(file_button)
 
     def create_new_file_window(self):
         from interfaces.managers import game_window_manager
         self.__create_name_popup = GiveNamePopup(self.rect.center, self.groups()[0])
         game_window_manager.add(self.__create_name_popup)
+
+    def confirm_overwrite_window(self, file_name):
+        from interfaces.managers import game_window_manager
+        self.__overwrite_file_popup = ConfirmPopup(self.rect.center, f"WARNING: '{file_name}'\n will be overwritten",
+                                                   file_name, self.groups()[0])
+        game_window_manager.add(self.__overwrite_file_popup)
 
 
 class Popup(base_interface.Window, ABC):
@@ -68,6 +85,30 @@ class Popup(base_interface.Window, ABC):
         super().__init__(pos, self.SIZE, sprite_group, static=False, color=self.COLOR, movable=False, top_window=True,
                          **kwargs)
         self.response = False
+
+
+class ConfirmPopup(Popup):
+    def __init__(self, pos, message, true_response, sprite_group, **kwargs):
+        super().__init__(pos, sprite_group, **kwargs)
+        self.__true_response = true_response
+        self.__init_widgets(message)
+
+    def __init_widgets(self, message):
+        for index, line in enumerate(message.split("\n")):
+            message_lbl = widgets.Label(util.Size(self.rect.width - 20, 25), color=self.COLOR, text=line, font_size=18)
+            self.add_widget((10, index * 18 + 5), message_lbl)
+
+        oke_button = widgets.Button(util.Size(self.rect.width / 2 - 25, 25), text="OK", font_size=25)
+        oke_button.add_key_event_listener(1, self.__set_response_true, types=["unpressed"])
+        self.add_widget((20, 60), oke_button)
+
+        cancel_button = widgets.Button(util.Size(self.rect.width / 2 - 25, 25), text="CANCEL", font_size=25)
+        cancel_button.add_key_event_listener(1, self._close_window, types=["unpressed"])
+        self.add_widget((self.rect.width - cancel_button.rect.width - 20, 60), cancel_button)
+
+    def __set_response_true(self):
+        self.response = self.__true_response
+        self._close_window()
 
 
 class GiveNamePopup(Popup):
